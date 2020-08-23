f5a7f17b1c5cfe07b53af10eeb5dfabbe23da972
buginux <wordbeyondyoung@gmail.com>
2020-08-23 09:49:01 +0800

    【Feature】财报 Runner 使用有误

--- zvt/recorders/eastmoney/finance_balance_sheet_runner.py
+++ zvt/recorders/eastmoney/finance_balance_sheet_runner.py
@@ -4,30 +4,41 @@ import logging
 import time
 
 from apscheduler.schedulers.background import BackgroundScheduler
+from zvt.informer.informer import WeWorkInformer
 
 from zvt.recorders.eastmoney.finance.china_stock_balance_sheet_recorder import ChinaStockBalanceSheetRecorder
-from zvt.recorders.eastmoney.finance.china_stock_income_statement_recorder import ChinaStockIncomeStatementRecorder
 from zvt import init_log
 
 logger = logging.getLogger(__name__)
 
 sched = BackgroundScheduler()
+wework = WeWorkInformer()
 
 
-@sched.scheduled_job('cron', hour=2, minute=00)
+@sched.scheduled_job('cron', day_of_week='tue,sat', hour=1, minute=00)
 def run():
+    err_count = 0
+
     while True:
         try:
-            ChinaStockBalanceSheetRecorder().run()
-            ChinaStockIncomeStatementRecorder().run()
+            ChinaStockBalanceSheetRecorder(sleeping_time=0.0).run()
+
+            wework.send_finished_message('资产负债表')
+            err_count = 0
             break
         except Exception as e:
-            logger.exception('finance runner 1 error:{}'.format(e))
+            err_count += 1
+
+            if err_count >= 10:
+                wework.send_message(f'资产负债表下载出错超过 {err_count} 次, 请检查...\n{e}')
+                err_count = 0
+
+            logger.exception('finance balance sheet runner error:{}'.format(e))
             time.sleep(60)
 
 
 if __name__ == '__main__':
-    init_log('eastmoney_balance_sheet.log')
+    init_log('eastmoney_finance_balance_sheet.log')
 
     run()
 
--- zvt/recorders/eastmoney/finance_factor_runner.py
+++ zvt/recorders/eastmoney/finance_factor_runner.py
@@ -6,7 +6,7 @@ import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from zvt.informer.informer import WeWorkInformer
 
-from zvt.recorders.eastmoney.finance.china_stock_balance_sheet_recorder import ChinaStockBalanceSheetRecorder
+from zvt.recorders.eastmoney.finance.china_stock_finance_factor_recorder import ChinaStockFinanceFactorRecorder
 from zvt import init_log
 
 logger = logging.getLogger(__name__)
@@ -15,25 +15,25 @@ sched = BackgroundScheduler()
 wework = WeWorkInformer()
 
 
-@sched.scheduled_job('cron', day_of_week='tue,sat', hour=1, minute=00)
+@sched.scheduled_job('cron', day_of_week='sun,thu', hour=1, minute=00)
 def run():
     err_count = 0
 
     while True:
         try:
-            ChinaStockBalanceSheetRecorder(sleeping_time=0.0).run()
+            ChinaStockFinanceFactorRecorder(sleeping_time=0.0).run()
 
-            wework.send_finished_message('资产负债表')
+            wework.send_finished_message('FinanceFactor')
             err_count = 0
             break
         except Exception as e:
             err_count += 1
 
             if err_count >= 10:
-                wework.send_message(f'资产负债表下载出错超过 {err_count} 次, 请检查...\n{e}')
+                wework.send_message(f'FinanceFactor 下载出错超过 {err_count} 次, 请检查...\n{e}')
                 err_count = 0
 
-            logger.exception('finance balance sheet runner error:{}'.format(e))
+            logger.exception('finance factor runner error:{}'.format(e))
             time.sleep(60)
 
 
